3734f47a464eec210838bf3597ac29bf3e11963c
houseofsteinert <14843079+StephanSteinert@users.noreply.github.com>
2019-01-25 13:48:22 +0100

    Created

---
+++ data-updates.lua
@@ -0,0 +1,9 @@
+data.raw.lamp["small-lamp"].fast_replaceable_group = "lamps"
+
+if data.raw.lamp["balloon-light"] ~= nil then
+   data.raw.lamp["balloon-light"].fast_replaceable_group = "lamps"
+end
+
+if data.raw.lamp["short-balloon-light"] ~= nil then
+   data.raw.lamp["short-balloon-light"].fast_replaceable_group = "lamps"
+end
