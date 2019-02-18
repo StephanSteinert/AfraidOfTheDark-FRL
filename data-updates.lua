5f523a83785c2d5e254fc2ebc1bcf330564eedd4
houseofsteinert <14843079+StephanSteinert@users.noreply.github.com>
2019-02-18 20:49:04 +0100

    All lamps are fast replacable

--- data-updates.lua
+++ data-updates.lua
@@ -1,9 +1,4 @@
-data.raw.lamp["small-lamp"].fast_replaceable_group = "lamps"
 
-if data.raw.lamp["balloon-light"] ~= nil then
-   data.raw.lamp["balloon-light"].fast_replaceable_group = "lamps"
-end
-
-if data.raw.lamp["short-balloon-light"] ~= nil then
-   data.raw.lamp["short-balloon-light"].fast_replaceable_group = "lamps"
+for _, lamp in pairs(data.raw.lamp) do
+    lamp.fast_replaceable_group = "lamps"
 end
